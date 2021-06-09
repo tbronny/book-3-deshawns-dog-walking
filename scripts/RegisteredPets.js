9a08ebd4eeb1a9d23da2db150205cec172d94912
tbronny <tristan.bron7@gmail.com>
2021-06-08 20:02:23 -0500

    added whos walking who

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,15 +1,35 @@
-import { getPets } from './database.js';
+import { getPets } from "./database.js"
+import { getWalkers } from "./database.js"
 
-const pets = getPets();
+document.addEventListener("click", (clickEvent) => {
+    const itemClicked = clickEvent.target
+    if (itemClicked.id.startsWith("pet")) {
+        const [, petId] = itemClicked.id.split("--")
+
+        // find method -- takes place of nesting loops..
+        const clickedPet = pets.find((pet) => pet.id === parseInt(petId))
+        const foundWalker = walkers.find(
+            (walker) => walker.id === clickedPet.walkerId
+        )
+        // end find method..
+
+        window.alert(
+            `${clickedPet.name} is being walker by ${foundWalker.name}`
+        )
+    }
+})
+
+const pets = getPets()
+const walkers = getWalkers()
 
 export const RegisteredPets = () => {
-    let petHTML = '<ul>';
+    let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`;
+        petHTML += `<li id ="pet--${pet.id}">${pet.name}</li>`
     }
 
-    petHTML += '</ul>';
+    petHTML += "</ul>"
 
-    return petHTML;
-};
+    return petHTML
+}
